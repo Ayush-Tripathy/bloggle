1953ff431a5871c49decbadbc2300ca063512873
Ayush Tripathy <ankitatripathy547@gmail.com>
2024-04-08 22:39:30 +0530

    Added favicon

--- pages/login.php
+++ pages/login.php
@@ -20,6 +20,7 @@ if (isset($_SESSION['user'])) {
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="icon" href="favicon.jpg" type="image/jpg">
   <title>Bloggle - Login</title>
   <?php
   include_once './utils/utils.php';
